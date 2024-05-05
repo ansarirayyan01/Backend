8632ff98f7dc012ac84df11bf73efcc7611cd4de
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-05-05 16:39:07 +0530

    export refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -238,4 +238,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 })
 
-export {registerUser, loginUser, logoutUser, }
+export {registerUser, loginUser, logoutUser, refreshAccessToken }
