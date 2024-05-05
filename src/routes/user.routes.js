393efa672c3acbfedd3043bd6cbe18e43e404c83
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-05-05 16:38:44 +0530

    typos in route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,5 +22,5 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser)
-router.route("/refresh-token".post(refreshAccessToken))
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
